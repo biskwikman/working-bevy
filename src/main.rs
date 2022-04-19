9c3209b35e984eea45c76b1fc78695656b25a2a9
Dan Henri <djosephhenri@gmail.com>
2022-04-19 15:57:34 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+//Sets things like window and imports game.
+
+// disable console on windows for release builds
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use bevy::prelude::{App, ClearColor, Color, Msaa, WindowDescriptor};
+use bevy::DefaultPlugins;
+use working::GamePlugin;
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa { samples: 1 })
+        .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
+        .insert_resource(WindowDescriptor {
+            width: 800.,
+            height: 600.,
+            title: "Working".to_string(), // ToDo
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(GamePlugin)
+        .run();
+}
